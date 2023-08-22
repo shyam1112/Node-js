0ebb95fbb7b8e1c36ccfa504e89c8b804fa81e91
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-08-22 13:03:32 +0530

    a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello node js i am ready to learn you..")
+
+const fs=require('fs');
+
+fs.writeFileSync('./thapa/bio.txt','Hello nodejs.. ');
+fs.appendFileSync('./thapa/bio.txt','Shyam Sardhara')
+const buf_data= fs.readFileSync('./thapa/bio.txt');
+
+console.log(buf_data.toString());
